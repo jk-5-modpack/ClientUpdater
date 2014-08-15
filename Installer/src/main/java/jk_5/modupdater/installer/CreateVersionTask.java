25c28d81af8fbb01ec636c0a1501ae4fd19ab544
jk-5 <jeffreykog@me.com>
2014-08-15 10:38:33 +0200

    Don't compare JsonStrings, but normal strings

--- Installer/src/main/java/jk_5/modupdater/installer/CreateVersionTask.java
+++ Installer/src/main/java/jk_5/modupdater/installer/CreateVersionTask.java
@@ -66,7 +66,7 @@ public class CreateVersionTask implements Runnable {
                 }
             }
             if(add){
-                if(e.asObject().get("url") != null && e.asObject().get("url").equals("http://files.minecraftforge.net/maven/")){
+                if(e.asObject().get("url") != null && e.asObject().get("url").asString().equals("http://files.minecraftforge.net/maven/")){
                     e.asObject().set("url", "http://repo1.maven.org/maven2/"); //Forge only hosts packed versions...
                 }
                 libs.add(e);
